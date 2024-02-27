14f3b26b6c3987a1b872598e7d7d075c7ba07b55
Dmytro <nirqqen97@gmail.com>
2024-02-27 16:13:34 +0100

    es-lint fix 1

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -80,6 +80,7 @@ const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
             isTransforming={false}
             transformationConfig={image.config}
             hasDownload={true}
+            setIsTransforming={undefined} // Явно указываем, что этот пропс не используется
           />
         </div>
 
